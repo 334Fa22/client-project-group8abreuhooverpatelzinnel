9746c2650fb9c000879a7c342c8b0cdc09ee6653
danielabreu2435 <danielabreu2435@gmail.com>
2022-10-12 16:31:16 -0500

    working hamburger

--- scripts/script.js
+++ scripts/script.js
@@ -7,5 +7,10 @@
 //*Hamburger menu function
 function hamburger()   {
   var menu = document.getElementById("menu-links");
-  
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+
 }
